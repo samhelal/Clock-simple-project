f3fb1459e0c01ef71cb2946852bd982aea0f74bc
samhelal <93845103+samhelal@users.noreply.github.com>
2023-09-09 23:37:02 +0300

    Update Clock-simple-project.cpp

--- Clock-simple-project.cpp
+++ Clock-simple-project.cpp
@@ -40,7 +40,9 @@ int main()
         {
             hours = 0;
         }
-        cout << hours << ":" << min << ":" << sec << endl;
+        cout <<  ( to_string (hours).size() == 1 ? "0" : "") << hours << " : " <<   //to add (0) before single number we change 
+                 ( to_string (min).size() == 1 ? "0" : "" ) << min << " : " <<   //the type first from int to string then use the  
+                 ( to_string (sec).size() == 1 ? "0" : ""  ) << sec << endl;  // size function to can count ( hh : mm : ss )
         Sleep(900);
     }
 }
